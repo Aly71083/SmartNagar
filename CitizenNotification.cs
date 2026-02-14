fdba7ceed9da2a569099ec5d4b6b85736de220fb
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:35:57 +0545

    Add files via upload

---
+++ CitizenNotification.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartNagar.Models
+{
+    public class CitizenNotification
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CitizenId { get; set; } = "";
+        public User? Citizen { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Title { get; set; } = "";
+
+        [Required, MaxLength(400)]
+        public string Message { get; set; } = "";
+
+        [MaxLength(50)]
+        public string Type { get; set; } = "General"; // ComplaintUpdate / Notice / General
+
+        public bool IsRead { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // optional: link to complaint
+        public int? ComplaintId { get; set; }
+    }
+}
