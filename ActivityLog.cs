805c3ab2f3032f69c5efa4c35d30d6c2da4d6cee
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:35:34 +0545

    Add files via upload

---
+++ ActivityLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmartNagar.Models
+{
+    public class ActivityLog
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; } = "";   // "User", "Notice", "Complaint"
+        public string Title { get; set; } = "";
+        public string Detail { get; set; } = "";
+
+        public bool IsRead { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
