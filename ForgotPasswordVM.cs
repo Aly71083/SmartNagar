2a33fb0f4b61e4139d1c4511e049d11f5767861b
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:36:55 +0545

    Add files via upload

---
+++ ForgotPasswordVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartNagar.ViewModels
+{
+    public class ForgotPasswordVM
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+    }
+}
