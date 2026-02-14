adca1e07f605dab399ef199b5433731f709aa9ed
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:36:16 +0545

    Add files via upload

---
+++ CitizenDashboardVM.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartNagar.ViewModels
+{
+    public class CitizenDashboardVM
+    {
+        public string FullName { get; set; } = "Citizen";
+        public int TotalComplaints { get; set; }
+        public int InProgress { get; set; }
+        public int Pending { get; set; }
+        public int Resolved { get; set; }
+    }
+}
