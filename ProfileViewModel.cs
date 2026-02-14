53b938a7bfd5c5d4d7974c34a10c32f383d86753
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:38:11 +0545

    Add files via upload

---
+++ ProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartNagar.ViewModels
+{
+    public class ProfileViewModel
+    {
+        [Required, MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [MaxLength(200)]
+        public string? Address { get; set; }
+
+        public string? TotalComplaints { get; set; }
+
+            public string? ResolvedComplaints { get; set; }
+    
+            public string? MemberYears { get; set; }
+
+    }
+}
