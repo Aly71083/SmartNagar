c1b0cf730960cdbc845b117968cf3623338efedd
Alisa Chalaune <walnutbrownie991@gmail.com>
2026-02-14 10:37:07 +0545

    Add files via upload

---
+++ HomeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartNagar.ViewModels
+{
+    public class HomeVM
+    {
+        public int ActiveCitizens { get; set; }      // from Users table
+        public int ResolvedComplaints { get; set; }  // placeholder until Complaint module (0, not random)
+        public int SatisfactionRate { get; set; }    // based on users: ActiveUsers/TotalUsers * 100
+        public string ServiceAvailability { get; set; } = "24/7";
+    }
+}
